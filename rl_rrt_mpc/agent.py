Fix nan gradient value bug in RBF surfaces. Add scaling functionality to acados OCP

rl_rrt_mpc/agent.py
@@ -343,13 +343,15 @@ class RLMPC(ci.ICOLAV):
             enc.start_display()
             # hf.plot_trajectory(waypoints, enc, color="green")
             hf.plot_trajectory(self._nominal_trajectory, enc, color="magenta")
-            for hazard in relevant_grounding_hazards:
+            for hazard in self._rel_polygons:
                 enc.draw_polygon(hazard, color="red", fill=False)
+
             ship_poly = hf.create_ship_polygon(ownship_state[0], ownship_state[1], ownship_state[2], kwargs["os_length"], kwargs["os_width"], 1.0, 1.0)
             # enc.draw_circle((ownship_state[1], ownship_state[0]), radius=40, color="yellow", alpha=0.4)
             enc.draw_polygon(ship_poly, color="pink")
             # enc.draw_circle((goal_state[1], goal_state[0]), radius=40, color="cyan", alpha=0.4)
 
+        # enc.save_image(name="enc_hazards", path=dp.figures, extension="pdf")
         if self._mpc.params.so_constr_type == mpc_params.StaticObstacleConstraint.CIRCULAR:
             self._mpc_rel_polygons = mapf.compute_smallest_enclosing_circle_for_polygons(self._rel_polygons, enc)
         elif self._mpc.params.so_constr_type == mpc_params.StaticObstacleConstraint.ELLIPSOIDAL:
@@ -359,8 +361,6 @@ class RLMPC(ci.ICOLAV):
             self._set_generator = sg.SetGenerator(P1, P2)
         elif self._mpc.params.so_constr_type == mpc_params.StaticObstacleConstraint.PARAMETRICSURFACE:
             self._mpc_rel_polygons = poly_tuple_list  # mapf.extract_boundary_polygons_inside_envelope(poly_tuple_list, enveloping_polygon, enc)
-        elif self._mpc.params.so_constr_type == mpc_params.StaticObstacleConstraint.TRIANGULARBOUNDARY:
-            self._mpc_rel_polygons = mapf.extract_boundary_polygons_inside_envelope(poly_tuple_list, enveloping_polygon, enc)
 
     def _update_mpc_so_polygon_input(self, state: np.ndarray, enc: Optional[senc.ENC] = None, show_plots: bool = False) -> None:
         """Updates the static obstacle constraint parameters to the MPC, based on the constraint type used.

rl_rrt_mpc/common/map_functions.py
@@ -13,10 +13,10 @@ from typing import Optional, Tuple
 import casadi as csd
 import geopandas as gpd
 import geopy.distance
-import matplotlib.path as mpath
 import matplotlib.pyplot as plt
 import numpy as np
 import rl_rrt_mpc.common.helper_functions as hf
+import rl_rrt_mpc.common.paths as dp
 import rl_rrt_mpc.common.rbf_casadi as rbf_casadi
 import rl_rrt_mpc.common.smallestenclosingcircle as smallestenclosingcircle
 import rl_rrt_mpc.gmm_em as gmm_em
@@ -676,15 +676,15 @@ def compute_multi_ellipsoidal_approximations_from_polygons(
 
 
 def compute_surface_approximations_from_polygons(
-    polygons: list, casadi_opts: dict, enc: Optional[senc.ENC] = None, safety_margins: Optional[list] = None, show_plots: bool = False
+    polygons: list, enc: Optional[senc.ENC] = None, safety_margins: Optional[list] = None, scale_data: bool = True, show_plots: bool = False
 ) -> list:
     """Computes smooth 2D surface approximations from the input polygon list.
 
     Args:
         polygons (list): List of shapely polygons
-        casadi_opts (dict): CasADi options used for the function generation.
         enc (Optional[senc.ENC], optional): ENC object. Defaults to None.
         safety_margins (Optional[list], optional): List of safety margins to buffer the polygon. Defaults to None.
+        scale_data (bool, optional): Whether to scale the data to within -1 and 1, for better numerical conditioning. Defaults to True.
         show_plots (bool, optional): Whether to show plots. Defaults to False.
 
     Returns:
@@ -696,6 +696,7 @@ def compute_surface_approximations_from_polygons(
     code_gen = csd.CodeGenerator("surface_functions")
     if show_plots:
         ax = plt.figure().add_subplot(111, projection="3d")
+        # ax2 = plt.figure().add_subplot(111, projection="3d")
         # ax.axis("equal")
         # ax3 = plt.figure().add_subplot(111)
 
@@ -712,10 +713,8 @@ def compute_surface_approximations_from_polygons(
                 0.5, cap_style=cap_style, join_style=join_style
             )
             for polygon in polygons:
-
                 coastline_original = polygon.intersection(original_polygon_boundary)
                 n_orig_boundary_points = len(coastline_original.exterior.coords.xy[0])
-                mask_unstructured = []
                 coastline = polygon.buffer(d_safe, cap_style=cap_style, join_style=join_style).intersection(original_polygon_boundary_d_safe)
                 y_poly_unstructured, x_poly_unstructured = coastline.exterior.coords.xy
 
@@ -745,37 +744,41 @@ def compute_surface_approximations_from_polygons(
                             if original_polygon_boundary_d_safe.buffer(buff_l, cap_style=cap_style, join_style=join_style).contains(geometry.Point(ycoord, xcoord)):
                                 x_poly_unstructured.append(xcoord)
                                 y_poly_unstructured.append(ycoord)
-                                mask_unstructured.append(-1.0)
+                                mask_unstructured.append(-10.0)
                     except AttributeError:
                         break
-                    if enc is not None and show_plots:
-                        enc.draw_polygon(coastline.buffer(buff_l, cap_style=cap_style, join_style=join_style), color="orange", fill=False)
+                    # if enc is not None and show_plots:
+                    #     enc.draw_polygon(coastline.buffer(buff_l, cap_style=cap_style, join_style=join_style), color="orange", fill=False)
 
-                # Add polygon boundary points
-                poly_min_east, poly_min_north, poly_max_east, poly_max_north = polygon.buffer(d_safe + 10.0, cap_style=cap_style, join_style=join_style).bounds
-                x_poly_unstructured.extend([poly_min_north, poly_max_north, poly_max_north, poly_min_north])
-                y_poly_unstructured.extend([poly_min_east, poly_min_east, poly_max_east, poly_max_east])
-                mask_unstructured.extend([-10.0, -10.0, -10.0, -10.0])
+                relevant_boundary = polygon.buffer(d_safe + 100.0).intersection(
+                    geometry.LineString(original_poly.buffer(d_safe + 100.0).exterior.coords).buffer(1.0, cap_style=cap_style, join_style=join_style)
+                )
+                y_boundary, x_boundary = relevant_boundary.exterior.coords.xy
+                n_boundary_points = 8
+                if len(y_boundary) < n_boundary_points:
+                    n_boundary_points = len(y_boundary)
+                elif len(y_boundary) > 300:
+                    n_boundary_points = 20
+                step = int(len(y_boundary) / n_boundary_points)
+                for i in range(0, len(y_boundary), step):
+                    x_poly_unstructured.append(x_boundary[i])
+                    y_poly_unstructured.append(y_boundary[i])
+                    mask_unstructured.append(-100.0)
 
-                # Add points in the middle between the polygon boundary points
-                # x_poly_unstructured.extend(
-                #     [poly_min_north + 0.5 * (poly_max_north - poly_min_north), poly_max_north, poly_min_north + 0.5 * (poly_max_north - poly_min_north), poly_min_north]
-                # )
-                # y_poly_unstructured.extend(
-                #     [poly_min_east, poly_min_east + 0.5 * (poly_max_east - poly_min_east), poly_max_east, poly_min_east + 0.5 * (poly_max_east - poly_min_east)]
-                # )
-                # mask_unstructured.extend([-100.0, -100.0, -100.0, -100.0])
-
-                # poly_min_east, poly_min_north, poly_max_east, poly_max_north = polygon.buffer(d_safe + 5000.0).bounds
-                # x_poly_unstructured.extend([poly_min_north, poly_max_north, poly_max_north, poly_min_north])
-                # y_poly_unstructured.extend([poly_min_east, poly_min_east, poly_max_east, poly_max_east])
-                # mask_unstructured.extend([-1000.0, -1000.0, -1000.0, -1000.0])
+                poly_min_east, poly_min_north, poly_max_east, poly_max_north = polygon.buffer(d_safe + 100.0, cap_style=cap_style, join_style=join_style).bounds
 
                 # rbf = scipyintp.RBFInterpolator(
                 #     np.array([x_poly_unstructured, y_poly_unstructured]).T, np.array(mask_unstructured), kernel="gaussian", epsilon=0.08, smoothing=1e-3
                 # )
+
+                if scale_data:
+                    map_xmin, map_ymin, map_xmax, map_ymax = enc.bbox
+                    for i in range(len(x_poly_unstructured)):  # pylint: disable=consider-using-enumerate
+                        x_poly_unstructured[i] = (x_poly_unstructured[i] - map_ymin) / (map_ymax - map_ymin)
+                        y_poly_unstructured[i] = (y_poly_unstructured[i] - map_xmin) / (map_xmax - map_xmin)
+
                 rbf = scipyintp.RBFInterpolator(
-                    np.array([x_poly_unstructured, y_poly_unstructured]).T, np.array(mask_unstructured), kernel="thin_plate_spline", epsilon=10.0, smoothing=1e-6
+                    np.array([x_poly_unstructured, y_poly_unstructured]).T, np.array(mask_unstructured), kernel="thin_plate_spline", epsilon=10.0, smoothing=1e-5
                 )
                 rbf_csd = rbf_casadi.RBFInterpolator(
                     np.array([x_poly_unstructured, y_poly_unstructured]).T,
@@ -784,18 +787,12 @@ def compute_surface_approximations_from_polygons(
                     rbf.powers,
                     rbf._shift,
                     rbf._scale,
-                    rbf.smoothing,
                     "thin_plate_spline",
                     rbf.epsilon,
                 )
                 x = csd.MX.sym("x", 2)
                 intp = rbf_csd(x.reshape((1, 2)))
-                if False:  # casadi_opts["jit"]:
-                    rbf_surface_func = csd.Function(
-                        "surface_func", [x.reshape((1, 2))], [intp], {"jit": True, "compiler": casadi_opts["compiler"], "jit_options": casadi_opts["jit_options"]}
-                    )
-                else:
-                    rbf_surface_func = csd.Function("so_surface_func_" + str(j) + "_" + safety_margin_str, [x.reshape((1, 2))], [intp])
+                rbf_surface_func = csd.Function("so_surface_func_" + str(j) + "_" + safety_margin_str, [x.reshape((1, 2))], [intp])
                 surfaces.append(rbf_surface_func)
 
                 grad_rbf = csd.gradient(rbf_surface_func(x.reshape((1, 2))), x.reshape((1, 2)))
@@ -803,17 +800,39 @@ def compute_surface_approximations_from_polygons(
 
                 code_gen.add(rbf_surface_func)
                 j += 1
+
                 if enc is not None and show_plots:
+                    # if j == 2:
+                    #     enc.draw_polygon(polygon.buffer(d_safe, cap_style=cap_style, join_style=join_style), color="black", fill=False)
+                    #     save_path = dp.figures
+                    #     enc.save_image(name="enc_island_polygon", path=save_path, extension="pdf")
+                    #     enc.save_image(name="enc_island_polygon", path=save_path, scale=2.0)
                     assert enc is not None
                     buffer = 0.0
-                    extra_north_coords = np.linspace(start=poly_min_north - buffer, stop=poly_max_north + buffer, num=100)
-                    extra_east_coords = np.linspace(start=poly_min_east - buffer, stop=poly_max_east + buffer, num=100)
-                    surface_points = np.zeros((100, 100))
-                    for i, north_coord in enumerate(extra_north_coords):
-                        for j, east_coord in enumerate(extra_east_coords):
-                            surface_points[i, j] = rbf_surface_func(np.array([north_coord, east_coord]).reshape(1, 2))
-                            # surface_points2[i, j] = polygon_surface2([north_coord, east_coord])
-                            # surface_points2[i, j] = spline_func([north_coord, east_coord], coeff).full()
+                    n_points = 150
+
+                    if scale_data:
+                        extra_north_coords = np.linspace(
+                            start=(poly_min_north - buffer - map_ymin) / (map_ymax - map_ymin),
+                            stop=(poly_max_north + buffer - map_ymin) / (map_ymax - map_ymin),
+                            num=n_points,
+                        )
+                        extra_east_coords = np.linspace(
+                            start=(poly_min_east - buffer - map_xmin) / (map_xmax - map_xmin),
+                            stop=(poly_max_east + buffer - map_xmin) / (map_xmax - map_xmin),
+                            num=n_points,
+                        )
+                    else:
+                        extra_north_coords = np.linspace(start=poly_min_north - buffer, stop=poly_max_north + buffer, num=n_points)
+                        extra_east_coords = np.linspace(start=poly_min_east - buffer, stop=poly_max_east + buffer, num=n_points)
+
+                    surface_points = np.zeros((n_points, n_points))
+                    surface_grad_points = np.zeros((n_points, n_points, 2))
+                    for i, east_coord in enumerate(extra_east_coords):
+                        for ii, north_coord in enumerate(extra_north_coords):
+                            point = np.array([north_coord, east_coord]).reshape(1, 2)
+                            surface_points[i, ii] = rbf_surface_func(point)
+                            surface_grad_points[i, ii, :] = grad_rbf_func(point).full().flatten()
 
                         # ax3.plot(extra_east_coords, surface_points2[i, :], "b")
                         # ax3.plot(np.array(y_poly_unstructured) - poly_min_east, mask_unstructured, "ro")
@@ -821,19 +840,30 @@ def compute_surface_approximations_from_polygons(
                         # plt.show(block=False)
                         # ax3.clear()
 
-                    xX, yY = np.meshgrid(extra_north_coords, extra_east_coords, indexing="ij")
+                    print(f"Number of gradient NaNs: {np.count_nonzero(np.isnan(surface_grad_points))}")
+                    yY, xX = np.meshgrid(extra_east_coords, extra_north_coords, indexing="ij")
 
                     fig, ax4 = plt.subplots()
-                    ax4.pcolormesh(xX, yY, surface_points, shading="gouraud")
-                    p = ax4.scatter(x_poly_unstructured, y_poly_unstructured, c=np.array(mask_unstructured), s=50, ec="k")
+                    ax4.pcolormesh(yY, xX, surface_points, shading="gouraud")
+                    p = ax4.scatter(y_poly_unstructured, x_poly_unstructured, c=np.array(mask_unstructured), s=50, ec="k")
                     fig.colorbar(p)
+                    ax4.set_xlabel("East")
+                    ax4.set_ylabel("North")
 
-                    ax.plot_surface(xX, yY, surface_points, rcount=200, ccount=200, cmap=cm.coolwarm)
-                    ax.set_xlabel("North")
-                    ax.set_ylabel("East")
+                    ax.plot_surface(yY, xX, surface_points, rcount=200, ccount=200, cmap=cm.coolwarm)
+                    ax.set_ylabel("North")
+                    ax.set_xlabel("East")
                     ax.set_zlabel("Mask")
+                    # ax.set_title("Spline surface")
+
+                    # ax2.plot_surface(xX, yY, surface_grad_points[:, :, 0], rcount=200, ccount=200, cmap=cm.coolwarm)
+                    # ax2.set_xlabel("North")
+                    # ax2.set_ylabel("East")
+                    # ax2.set_zlabel("Mask")
+                    # ax2.set_title("Spline surface gradient x")
                     plt.show(block=False)
                     ax.clear()
+                    # ax2.clear()
         surfaces_list.append(surfaces)
         code_gen.generate()
     return surfaces_list

rl_rrt_mpc/common/rbf_casadi.py
@@ -15,7 +15,7 @@ def linear(r: csd.MX) -> csd.MX:
 
 
 def thin_plate_spline(r: csd.MX) -> csd.MX:
-    return r**2 * csd.log(r + 0.000001)
+    return r**2 * csd.log(r)
 
 
 def cubic(r: csd.MX) -> csd.MX:
@@ -93,7 +93,6 @@ class RBFInterpolator:
         powers: np.ndarray,
         shift: np.ndarray,
         scale: np.ndarray,
-        smoothing: float = 0.0,
         kernel: str = "thin_plate_spline",
         epsilon: float = 1.0,
     ) -> None:
@@ -106,7 +105,6 @@ class RBFInterpolator:
             powers (np.ndarray): Exponents for the polynomial interpolant.
             shift (np.ndarray): Shifts the polynomial domain for numerical stability.
             scale (np.ndarray): Scales the polynomial domain for numerical stability. Defaults to None.
-            smoothing (float): Smoothing parameter for the RBF interpolant. Defaults to 0.0.
             kernel (str): Name of the RBF kernel used. Defaults to "thin_plate_spline".
             epsilon (float): Scaling parameter for the RBF kernel.
         """
@@ -118,7 +116,6 @@ class RBFInterpolator:
         self.d: np.ndarray = d
         self.p: int = self.y.shape[0]
         self.r: int = self.powers.shape[0]
-        self.smoothing: float = smoothing
         self.kernel_func = NAME_TO_FUNC[kernel]
         self.epsilon: float = epsilon
         self.yeps: np.ndarray = y * epsilon

rl_rrt_mpc/mpc/acados_mpc.py
@@ -63,7 +63,8 @@ class AcadosMPC:
         self._x_warm_start: np.ndarray = np.array([])
         self._u_warm_start: np.ndarray = np.array([])
         self._initialized = False
-        self._map_bbox: Tuple[int, int, int, int] = (0, 0, 0, 0)  # In east-north coordinates
+        self._map_pos_min: np.ndarray = np.array([])
+        self._map_pos_max: np.ndarray = np.array([])
 
         self._p_fixed: csd.MX = csd.MX.sym("p_fixed", 0)
         self._p_adjustable: csd.MX = csd.MX.sym("p_adjustable", 0)
@@ -204,6 +205,7 @@ class AcadosMPC:
         psi = trajectory[2, :]
         psi = np.unwrap(np.concatenate(([xs[2]], psi)))[1:]
         trajectory[2, :] = psi
+        trajectory[:2, :] = mf.scale_min_max(trajectory[:2, :], self._map_pos_min, self._map_pos_max)
         return inputs, trajectory, lower_slacks, upper_slacks
 
     def _update_ocp(self, nominal_trajectory: np.ndarray, nominal_inputs: Optional[np.ndarray], xs: np.ndarray, do_list: list, so_list: list) -> None:
@@ -247,7 +249,8 @@ class AcadosMPC:
             - enc (senc.ENC): ENC object.
 
         """
-        self._map_bbox = enc.bbox
+        self._map_pos_min = np.array([enc.bbox[1], enc.bbox[0]])
+        self._map_pos_max = np.array([enc.bbox[3], enc.bbox[2]])
         self._acados_ocp.model = self._model.as_acados()
         ship_vertices = self._model.params().ship_vertices
         self._acados_ocp.dims.N = int(self._params.T / self._params.dt)
@@ -309,15 +312,15 @@ class AcadosMPC:
         self._acados_ocp.constraints.ubu = ubu
 
         # State constraints
-        self._acados_ocp.constraints.idxbx_0 = np.array(range(nx))
-        self._acados_ocp.constraints.lbx_0 = lbx
-        self._acados_ocp.constraints.ubx_0 = ubx
+        self._acados_ocp.constraints.idxbx_0 = np.array([0, 1, 2, 3, 4, 5])
+        self._acados_ocp.constraints.lbx_0 = lbx[self._acados_ocp.constraints.idxbx_0]
+        self._acados_ocp.constraints.ubx_0 = ubx[self._acados_ocp.constraints.idxbx_0]
 
-        self._acados_ocp.constraints.idxbx = np.array([0, 1, 3, 4, 5])
+        self._acados_ocp.constraints.idxbx = np.array([3, 4, 5])
         self._acados_ocp.constraints.lbx = lbx[self._acados_ocp.constraints.idxbx]
         self._acados_ocp.constraints.ubx = ubx[self._acados_ocp.constraints.idxbx]
 
-        self._acados_ocp.constraints.idxbx_e = np.array([0, 1, 3, 4, 5])
+        self._acados_ocp.constraints.idxbx_e = np.array([3, 4, 5])
         self._acados_ocp.constraints.lbx_e = lbx[self._acados_ocp.constraints.idxbx_e]
         self._acados_ocp.constraints.ubx_e = ubx[self._acados_ocp.constraints.idxbx_e]
 
@@ -394,7 +397,7 @@ class AcadosMPC:
             list: List of static obstacle constraints at the current stage in the OCP.
         """
         epsilon = 1e-6
-        so_constr_list = []
+        so_constr_list: list = []
         if self._params.max_num_so_constr == 0:
             return so_constr_list
 
@@ -430,7 +433,7 @@ class AcadosMPC:
                     so_constr_list.append(csd.log(1 + epsilon) - csd.log(p_diff_do_frame.T @ weights @ p_diff_do_frame + epsilon))
 
             elif self._params.so_constr_type == parameters.StaticObstacleConstraint.PARAMETRICSURFACE:
-                so_surfaces = mapf.compute_surface_approximations_from_polygons(so_list, enc)[0]
+                so_surfaces = mapf.compute_surface_approximations_from_polygons(so_list, enc, safety_margins=[self._params.d_safe_so], scale_data=True)[0]
                 n_so = len(so_surfaces)
                 for j in range(self._params.max_num_so_constr):
                     if j < n_so:
@@ -465,9 +468,7 @@ class AcadosMPC:
             do_constr_list.append(csd.log(1 + epsilon) - csd.log(p_diff_do_frame.T @ weights @ p_diff_do_frame + epsilon))
         return do_constr_list
 
-    def create_parameter_values(
-        self, nominal_trajectory: np.ndarray, xs: np.ndarray, do_list: list, so_list: list, stage_idx: int, enc: Optional[senc.ENC] = None
-    ) -> np.ndarray:
+    def create_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, do_list: list, so_list: list, stage_idx: int, enc: senc.ENC = None) -> np.ndarray:
         """Creates the parameter vector values for a stage in the OCP, which is used in the cost function and constraints.
 
         Args:
@@ -488,6 +489,9 @@ class AcadosMPC:
             x_ref_stage = nominal_trajectory[0:2, stage_idx]
         else:
             x_ref_stage = nominal_trajectory[:6, stage_idx]
+
+        x_ref_stage[:2] = mf.normalize_min_max(x_ref_stage[:2], self._map_pos_min, self._map_pos_max)
+
         fixed_parameter_values.extend(x_ref_stage.tolist())
         fixed_parameter_values.append(self._params.gamma)
 
@@ -499,7 +503,7 @@ class AcadosMPC:
 
         return np.concatenate((adjustable_params, np.array(fixed_parameter_values)))
 
-    def _create_fixed_so_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, so_list: list, enc: Optional[senc.ENC] = None) -> np.ndarray:
+    def _create_fixed_so_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, so_list: list, enc: senc.ENC) -> np.ndarray:
         """Creates the fixed parameter values for the static obstacle constraints.
 
         Args:
@@ -520,7 +524,7 @@ class AcadosMPC:
                 fixed_so_parameter_values.extend([c[0], c[1], *A.flatten().tolist()])
         return np.array(fixed_so_parameter_values)
 
-    def _update_so_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, so_list: list, enc: Optional[senc.ENC] = None) -> list:
+    def _update_so_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, so_list: list, enc: senc.ENC) -> list:
         """Updates the parameter values for the static obstacle constraints in case of changing constraints.
 
         Args:
@@ -538,7 +542,7 @@ class AcadosMPC:
             self._p_fixed_so_values = np.concatenate((A.flatten(), b.flatten()), axis=0)
         return self._p_fixed_so_values.tolist()
 
-    def _create_do_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, do_list: list, stage_idx: int, enc: Optional[senc.ENC] = None) -> list:
+    def _create_do_parameter_values(self, nominal_trajectory: np.ndarray, xs: np.ndarray, do_list: list, stage_idx: int, enc: senc.ENC) -> list:
         """Updates the parameter values for the dynamic obstacle constraints in case of changing constraints.
 
         Args:
@@ -558,7 +562,9 @@ class AcadosMPC:
             t = stage_idx * dt
             if i < n_do:
                 (ID, state, cov, length, width) = do_list[i]
+                next_state = [state[0] + t * state[2], state[1] + t * state[3], state[2], state[3], length, width]
+                next_state[:2] = mf.normalize_min_max(next_state[:2], self._map_pos_min, self._map_pos_max)
                 do_parameter_values.extend([state[0] + t * state[2], state[1] + t * state[3], state[2], state[3], length, width])
             else:
-                do_parameter_values.extend([self._map_bbox[1], self._map_bbox[0], 0.0, 0.0, 5.0, 2.0])
+                do_parameter_values.extend([0.0, 0.0, 0.0, 0.0, 5.0, 2.0])
         return do_parameter_values