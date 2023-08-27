b11c6d17fc0c958ebd5ff6c9d6fe402e7d4c470d
trymte <tengesdal1994@gmail.com>
2023-08-27 14:40:30 +0200

    Update test rlrrtmpc file

--- tests/test_rl_rrt_mpc.py
+++ tests/test_rl_rrt_mpc.py
@@ -14,6 +14,15 @@ if __name__ == "__main__":
     scenario_generator = ScenarioGenerator()
     scenario_episode_list, enc = scenario_generator.generate(config_file=config_file)
 
+    # Vil kunne
+    # 1: lese inn mappe med AIS data som kan parses til ScenarioConfig-objekter med n_episodes og moglegheit for å adde randomgenererte båtar
+    # 2: Generere/hente ut shapefiler for alle kart for alle scenario-config objekt som skal brukast
+    # 3: Simulere n_episodar for kvart scenario, der own-ship har random starttilstand og sluttilstand, og alle andre båtar har AIS-trajectory eller varierande random trajectory. Skal kunne velge random control policy eller spesifikk feks RLRRTMPC policy.
+    # 4: Legg til moglegheit for å terminere simuleringa viss OS kræsjer
+    # 5: Lagre simuleringsdata (s_k, a_k, r_k+1, s_k+1, done_k+1) for alle episodar, og lagre i ein mappe med navn som er unikt for scenarioet.
+    # 6: Last opp eller direkte bruk simuleringsdata i ein replay buffer for å trene policyen til konvergens.
+    # 7: Test trent policy på testdata frå tilsvarande scenario (samme geografi som brukt i trening) og lagre resultatet i ein mappe med navn som er unikt for scenarioet.
+
     simulator = Simulator()
     evaluator = Evaluator()
     evaluator.set_enc(enc)
@@ -31,7 +40,6 @@ if __name__ == "__main__":
 
             evaluator.set_vessel_data(vessels)
             results = evaluator.evaluate()
-
             # rlrrtmpc.train(scenario_episode=scenario_episode, enc=enc)
 
             ep_execution_time = time.time() - ep_start_time
@@ -46,5 +54,7 @@ if __name__ == "__main__":
 
         if converged:
             break
+
+    # apply on test data
 
     print("done")
