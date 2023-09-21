814ec8be2f5860db969f1df991e0ca2dbf4adb35
Trym Tengesdal <tengesdal1994@gmail.com>
2023-09-21 12:13:25 +0200

    separate rrt functionality in separate repo

--- rl_rrt_mpc/rrt_star_lib/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!(
-        "cargo:rustc-link-arg=-Wl,-rpath,/Library/Developer/CommandLineTools/Library/Frameworks"
-    );
-    pyo3_build_config::add_extension_module_link_args();
-}
