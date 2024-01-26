a4937c73eb0e74f65cf700df05d9f27b58bf36cc
Trym Tengesdal <tengesdal1994@gmail.com>
2024-01-26 12:09:32 +0100

    Update scenarios and training data script

--- tests/generate_training_data.py
+++ tests/generate_training_data.py
@@ -7,13 +7,18 @@ import rl_rrt_mpc.common.paths as rl_dp
 if __name__ == "__main__":
     scenario_choice = 0
     if scenario_choice == 0:
-        scenario_name = "rlmpc_scenario_easy_cr_ss"
-        config_file = rl_dp.scenarios / "rlmpc_scenario_easy_cr_ss.yaml"
+        scenario_name = "rlmpc_scenario_cr_ss"
+        config_file = rl_dp.scenarios / (scenario_name + ".yaml")
     elif scenario_choice == 1:
-        scenario_name = "rlmpc_scenario_easy_headon_no_hazards"
+        scenario_name = "rlmpc_scenario_head_on_channel"
         config_file = rl_dp.scenarios / "rlmpc_scenario_easy_headon_no_hazards.yaml"
 
     scenario_generator = cs_sm.ScenarioGenerator(seed=0)
+
+    scen = scenario_generator.load_scenario_from_folder(
+        rl_dp.scenarios / "training_data" / scenario_name, scenario_name
+    )
+
     scenario_data = scenario_generator.generate(
         config_file=config_file,
         new_load_of_map_data=False,
