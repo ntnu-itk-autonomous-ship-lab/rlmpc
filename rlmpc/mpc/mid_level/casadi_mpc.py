Fix sensitivity calculation and rank deficient ineq jac

rlmpc/mpc/mid_level/casadi_mpc.py
@@ -294,7 +294,7 @@ class CasadiMPC:
         if self._so_surfaces:
             max_num_so_constr = len(self._so_surfaces) if self._params.max_num_so_constr > 0 else 0
 
-        n_bx_slacks = len(self._idx_bx_constr)
+        n_bx_slacks = len(self._idx_slacked_bx_constr)
         slacks = np.zeros(
             (n_bx_slacks + max_num_so_constr + n_colregs_zones * self._params.max_num_do_constr_per_zone) * (N + 1)
         )
@@ -785,8 +785,8 @@ class CasadiMPC:
         g_eq_list.append(x_0 - x_k)
         p_fixed.append(x_0)
 
-        self._idx_bx_constr = np.array([0, 1, 2, 3])
-        n_bx_slacks = self._idx_bx_constr.shape[0]
+        self._idx_slacked_bx_constr = np.array([0, 1, 2, 3, 4, 5])
+        n_bx_slacks = self._idx_slacked_bx_constr.shape[0]
         sigma_bx_k = csd.MX.sym(
             "sigma_bx_0",
             n_bx_slacks,
@@ -936,8 +936,12 @@ class CasadiMPC:
             hu.append(u_k - ubu_k)  # u_k <= ubu
             hs_bx.append(-sigma_bx_k)  # 0 <= sigma_bx_k
             hs_bx.append(sigma_bx_k - approx_inf)  # sigma_bx_k <= inf
-            hx.append(lbx_k[self._idx_bx_constr] - x_k[self._idx_bx_constr] - sigma_bx_k)  # lbx - sigma_bx <= x_k
-            hx.append(x_k[self._idx_bx_constr] - sigma_bx_k - ubx_k[self._idx_bx_constr])  # x_k <= ubx + sigma_bx
+            hx.append(
+                lbx_k[self._idx_slacked_bx_constr] - x_k[self._idx_slacked_bx_constr] - sigma_bx_k
+            )  # lbx - sigma_bx <= x_k
+            hx.append(
+                x_k[self._idx_slacked_bx_constr] - sigma_bx_k - ubx_k[self._idx_slacked_bx_constr]
+            )  # x_k <= ubx + sigma_bx
 
             slack_penalty_cost = W_bx.T @ sigma_bx_k
 
@@ -1047,16 +1051,15 @@ class CasadiMPC:
 
         J *= dt
 
-        # -1 - sigma <= x <= 1 + sigma
-        # sigma = 1 =>
-        # -1 - sigma - x <= 0
-        # x - 1 - sigma <= 0
-
         # Terminal costs and constraints
         hs_bx.append(-sigma_bx_k)  # 0 <= sigma_bx_k
         hs_bx.append(sigma_bx_k - approx_inf)  # sigma_bx_k <= 1e12 = inf
-        hx.append(lbx_k[self._idx_bx_constr] - x_k[self._idx_bx_constr] - sigma_bx_k)  # lbx - sigma_bx <= x_k
-        hx.append(x_k[self._idx_bx_constr] - sigma_bx_k - ubx_k[self._idx_bx_constr])  # x_k <= ubx + sigma_bx
+        hx.append(
+            lbx_k[self._idx_slacked_bx_constr] - x_k[self._idx_slacked_bx_constr] - sigma_bx_k
+        )  # lbx - sigma_bx <= x_k
+        hx.append(
+            x_k[self._idx_slacked_bx_constr] - sigma_bx_k - ubx_k[self._idx_slacked_bx_constr]
+        )  # x_k <= ubx + sigma_bx
         slack_penalty_cost = W_bx.T @ sigma_bx_k
         if max_num_so_constr > 0:
             sigma_so_k = csd.MX.sym(
@@ -1383,7 +1386,7 @@ class CasadiMPC:
         fixed_parameter_values.extend(so_parameter_values)
 
         max_num_so_constr = min(len(self._so_surfaces), self._params.max_num_so_constr)
-        n_bx_slacks = len(self._idx_bx_constr)
+        n_bx_slacks = len(self._idx_slacked_bx_constr)
         slack_size = n_bx_slacks + max_num_so_constr + n_colregs_zones * self._params.max_num_do_constr_per_zone
         W = self._params.w_L1 * np.ones(slack_size)
         fixed_parameter_values.extend(W.tolist())
@@ -1625,6 +1628,7 @@ class CasadiMPC:
         if not second_order:
             sens = mpc_common.NLPSensitivities.from_dict(output_dict)
             self._sensitivities = sens
+            # self._sensitivities.generate_c_functions(only_da_dp=True)
             return sens
 
         # Generate sensitivity of z_bar (contains perturbation as first element, with u0 as a fixed parameter)
@@ -2056,6 +2060,11 @@ class CasadiMPC:
         nlp_hess = self._nlp_hess_lag(soln["x"], parameter_values, 1.0, lam_g).full()
         nlp_hess_rank = np.linalg.matrix_rank(nlp_hess)
 
+        dlag_dw = self._sensitivities.dlag_dw(
+            soln["x"], soln["lam_g"], self._p_fixed_values, self._p_adjustable_values
+        ).full()
+        dlag_dw_norm = np.linalg.norm(dlag_dw, ord=2)
+
         arg_max_box_constr = np.argmax(g_ineq_bx_vals)
         max_box_constr = np.max(g_ineq_bx_vals)
 
@@ -2071,5 +2080,5 @@ class CasadiMPC:
             arg_max_so_constr, max_so_constr = np.argmax(so_constr_vals), np.max(so_constr_vals)
 
         print(
-            f"Mid-level COLAV: \n\t- Runtime: {t_solve} \n\t- Cost: {cost_val} \n\t- Slacks (max, argmax): ({max_sigma}, {arg_max_sigma}) \n\t- Box constraints (max, argmax): ({max_box_constr, arg_max_box_constr}) \n\t- Static obstacle constraints (max, argmax): ({max_so_constr}, {arg_max_so_constr}) \n\t- Dynamic obstacle constraints (max, argmax): ({max_do_constr}, {arg_max_do_constr})\n\t- Equality constraints jac (max_rank, rank): {max_g_eq_jac_rank, g_eq_jac_rank} \n\t- Inequality constraints jac (max_rank, rank): {max_g_ineq_jac_rank, g_ineq_jac_rank}\n\t- Hessian (max_rank, rank): {nlp_hess.shape[0], nlp_hess_rank}"
+            f"Mid-level COLAV: \n\t- Runtime: {t_solve} \n\t- Cost: {cost_val} \n\t- Slacks (max, argmax): ({max_sigma}, {arg_max_sigma}) \n\t- Box constraints (max, argmax): ({max_box_constr, arg_max_box_constr}) \n\t- Static obstacle constraints (max, argmax): ({max_so_constr}, {arg_max_so_constr}) \n\t- Dynamic obstacle constraints (max, argmax): ({max_do_constr}, {arg_max_do_constr})\n\t- Equality constraints jac (max_rank, rank): {max_g_eq_jac_rank, g_eq_jac_rank} \n\t- Inequality constraints jac (max_rank, rank): {max_g_ineq_jac_rank, g_ineq_jac_rank}\n\t- Hessian (max_rank, rank): {nlp_hess.shape[0], nlp_hess_rank} \n\t- ||dlag_dw||_2: {dlag_dw_norm} \n"
         )

rlmpc/mpc/parameters.py
@@ -192,11 +192,11 @@ class MidlevelMPCParams(IParams):
         Args:
             adjustable (np.ndarray): Array of adjustable parameters.
         """
-        self.Q_p = np.clip(np.diag(adjustable[:3]), np.diag([1e-6, 1e-6, 1e-6]), np.diag([1e3, 1e3, 1e3]))
-        self.alpha_app_course = np.clip(adjustable[3:5], np.array([1e-6, 1e-6]), np.array([1e3, 1e3]))
-        self.alpha_app_speed = np.clip(adjustable[5:7], np.array([1e-6, 1e-6]), np.array([1e3, 1e3]))
-        self.K_app_course = np.clip(adjustable[7], 0.00001, 1e3)
-        self.K_app_speed = np.clip(adjustable[8], 0.00001, 1e3)
+        self.Q_p = np.clip(np.diag(adjustable[:3]), np.diag([1e-4, 1e-4, 1e-4]), np.diag([1e3, 1e3, 1e3]))
+        self.alpha_app_course = np.clip(adjustable[3:5], np.array([1e-6, 0.000001]), np.array([1e3, 1.0]))
+        self.alpha_app_speed = np.clip(adjustable[5:7], np.array([1.0, 0.00001]), np.array([1e3, 1.0]))
+        self.K_app_course = np.clip(adjustable[7], 0.001, 1e3)
+        self.K_app_speed = np.clip(adjustable[8], 0.001, 1e3)
         self.alpha_cr = np.clip(adjustable[9:11], np.array([1e-6, 1e-6]), np.array([1.0, 1.0]))
         self.y_0_cr = np.clip(adjustable[11], -1e4, 1e4)
         self.alpha_ho = np.clip(adjustable[12:14], np.array([1e-6, 1e-6]), np.array([1.0, 1.0]))
@@ -205,5 +205,5 @@ class MidlevelMPCParams(IParams):
         self.x_0_ot = np.clip(adjustable[17], -1e4, 1e4)
         self.y_0_ot = np.clip(adjustable[18], -1e4, 1e4)
         self.d_attenuation = np.clip(adjustable[19], 1.0, 1e4)
-        self.w_colregs = np.clip(adjustable[20:23], np.array([1e-6, 1e-6, 1e-6]), np.array([1e4, 1e4, 1e4]))
+        self.w_colregs = np.clip(adjustable[20:23], np.array([1e-4, 1e-4, 1e-4]), np.array([1e4, 1e4, 1e4]))
         self.r_safe_do = np.clip(adjustable[23], 1.0, 1e4)

rlmpc/sac.py
@@ -25,16 +25,17 @@ import stable_baselines3.common.noise as sb3_noise
 import torch as th
 from colav_simulator.gym.environment import COLAVEnvironment
 from gymnasium import spaces
-from stable_baselines3.common.distributions import (
-    SquashedDiagGaussianDistribution, StateDependentNoiseDistribution)
+from stable_baselines3.common.distributions import SquashedDiagGaussianDistribution, StateDependentNoiseDistribution
 from stable_baselines3.common.preprocessing import get_action_dim
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
-from stable_baselines3.common.type_aliases import (GymEnv, MaybeCallback,
-                                                   Schedule)
-from stable_baselines3.common.utils import (get_parameters_by_name,
-                                            get_schedule_fn, polyak_update,
-                                            set_random_seed,
-                                            update_learning_rate)
+from stable_baselines3.common.type_aliases import GymEnv, MaybeCallback, Schedule
+from stable_baselines3.common.utils import (
+    get_parameters_by_name,
+    get_schedule_fn,
+    polyak_update,
+    set_random_seed,
+    update_learning_rate,
+)
 from stable_baselines3.sac.policies import BasePolicy, ContinuousCritic
 from torch.nn import functional as F
 
@@ -846,20 +847,17 @@ class SAC(opa.OffPolicyAlgorithm):
                 p_fixed = actor_info["p_fixed"]
                 z = np.concatenate((soln["x"], soln["lam_g"]), axis=0, dtype=np.float32)
 
-                dr_dz = sens.dr_dz(z, p_fixed, p).full()
-                dr_dp = sens.dr_dp(z, p_fixed, p).full()
-                dz_dp = -np.linalg.inv(dr_dz) @ dr_dp
-                da_dp = dz_dp[self.actor.action_indices, :]
+                da_dp = sens.da_dp(z, p_fixed, p).full()
                 da_dp = th.from_numpy(da_dp).float()
                 d_log_pi_dp = (cov @ (sampled_actions[b] - replay_data.actions[b]).reshape(-1, 1)).T @ da_dp
                 d_log_pi_da = cov @ (sampled_actions[b] - replay_data.actions[b])
                 df_repar_dp = da_dp
-                #
+
                 dQ_da = th.autograd.grad(min_qf_pi_sampled[b], sampled_actions, create_graph=True)[0][b]
                 actor_grads[b] = ent_coef * d_log_pi_dp + (ent_coef * d_log_pi_da - dQ_da) @ df_repar_dp
                 actor_losses[b] = ent_coef * log_prob_sampled[b] - min_qf_pi_sampled[b]
             print("Actor gradient computation time: ", time.time() - t_now)
-            self.actor.update_params(actor_grads.mean(dim=0) * self.lr_schedule(self._current_progress_remaining))
+            self.actor.update_params(-actor_grads.mean(dim=0) * self.lr_schedule(self._current_progress_remaining))
 
             if gradient_step % self.target_update_interval == 0:
                 polyak_update(self.critic.parameters(), self.critic_target.parameters(), self.tau)

tests/test_gym_sac_rlmpc.py
@@ -134,34 +134,38 @@ if __name__ == "__main__":
         policy,
         env,
         policy_kwargs=policy_kwargs,
-        buffer_size=100,
+        buffer_size=1000,
         learning_starts=0,
-        batch_size=2,
-        gradient_steps=2,
+        batch_size=1,
+        gradient_steps=1,
         train_freq=(2, "step"),
         device="cpu",
         tensorboard_log=log_dir,
         verbose=1,
     )
 
-    model.learn(total_timesteps=100, progress_bar=True)
+    total_timesteps = 100
+    model.learn(total_timesteps=total_timesteps, progress_bar=True, callback=CallbackList([eval_callback]))
     mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
     print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
 
     record = True
     if record:
-        video_path = rl_dp.animations / "demo.mp4"
+        video_path = rl_dp.animations / "test_sac_rlmpc.mp4"
         env = gym.wrappers.RecordVideo(env, video_path.as_posix(), episode_trigger=lambda x: x == 0)
 
-    env.reset(seed=1)
-    frames = []
-    for i in range(250):
-        obs, reward, terminated, truncated, info = env.step(np.array([-0.2, 0.0]))
+    n_episodes = 1
+    for i in range(n_episodes):
+        obs, _ = env.reset(seed=1)
+        done = False
+        frames = []
+        while not done:
+            action = model.predict_with_mpc(obs)
+            obs, reward, terminated, truncated, info = env.step(action)
 
-        frames.append(env.render())
+            frames.append(env.render())
 
-        if terminated or truncated:
-            env.reset()
+            done = terminated or truncated
 
     env.close()
 