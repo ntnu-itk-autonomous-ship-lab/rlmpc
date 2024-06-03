Fix bug in actor grad computation

rlmpc/policies.py
@@ -66,8 +66,8 @@ class MPCParameterDNN(th.nn.Module):
             "w_colregs": 3,
             "r_safe_do": 1,
         }
-        self.parameter_weights = 10.0 * th.diag([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0])
-        self.action_weight = 100.0 * th.diag([1.0, 1.0])
+        self.parameter_weights = 10.0 * th.diag(th.Tensor([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0]))
+        self.action_weight = 100.0 * th.diag(th.Tensor([1.0, 1.0]))
         self.mpc_cost_val_scaling = 0.0001
         self.human_preference_cost_val_scaling = 0.0001
 
@@ -77,7 +77,7 @@ class MPCParameterDNN(th.nn.Module):
             self.parameter_indices[param] = offset
             offset += self.out_parameter_lengths[param]
 
-        self.num_params = offset
+        self.num_output_params = offset
         self.param_list = param_list
         self.hidden_sizes = hidden_sizes
         self.activation_fn = activation_fn()
@@ -90,12 +90,11 @@ class MPCParameterDNN(th.nn.Module):
         for size in hidden_sizes:
             self.layers.append(th.nn.Linear(prev_size, size))
             prev_size = size
-        self.layers.append(th.nn.Linear(prev_size, self.num_params))
+        self.layers.append(th.nn.Linear(prev_size, self.num_output_params))
         self.tanh = th.nn.Tanh()
+        self.num_params = sum(p.numel() for p in self.parameters())
 
-    def forward(self, features: th.Tensor, current_params: th.Tensor, prev_action: th.Tensor) -> th.Tensor:
-        input_tensor = th.cat([features, current_params, prev_action], dim=-1)
-        x = input_tensor
+    def forward(self, x: th.Tensor) -> th.Tensor:
         for layer in self.layers[:-1]:
             x = layer(x)
             x = self.activation_fn(x)
@@ -131,16 +130,26 @@ class MPCParameterDNN(th.nn.Module):
             params[param] = x_param_new
         return params
 
-    def unnormalize(self, x: th.Tensor) -> np.ndarray:
-        """Unnormalize the DNN output tensor.
+    def save(self, path: pathlib.Path) -> None:
+        """Save the DNN model to a file.
 
         Args:
-            x (th.Tensor): The DNN output tensor
+            path (pathlib.Path): The path to save the model to
+        """
+        th.save(self.state_dict(), path)
+
+    def unnormalize(self, x: th.Tensor | np.ndarray) -> np.ndarray:
+        """Unnormalize the DNN output.
+
+        Args:
+            x (th.Tensor | np.ndarray): The DNN output
 
         Returns:
             np.ndarray: The unnormalized output as a numpy array
         """
-        x_unnorm = th.zeros_like(x)
+        if isinstance(x, th.Tensor):
+            x = x.detach().numpy()
+        x_unnorm = np.zeros_like(x)
         for param in self.param_list:
             param_range = self.out_parameter_ranges[param]
             param_length = self.out_parameter_lengths[param]
@@ -151,7 +160,7 @@ class MPCParameterDNN(th.nn.Module):
                 x_param[j] = csmf.linear_map(x_param[j], (-1.0, 1.0), tuple(param_range))
             x_unnorm[pindx : pindx + param_length] = x_param
 
-        return x_unnorm.detach().numpy()
+        return x_unnorm
 
     def normalize(self, p: th.Tensor) -> th.Tensor:
         """Normalize the input parameter (not increment) tensor.
@@ -174,30 +183,55 @@ class MPCParameterDNN(th.nn.Module):
             p_norm[pindx : pindx + param_length] = p_param
         return p_norm
 
-    def loss_function(
-        self,
-        param_increment: th.Tensor,
-        prev_action: th.Tensor,
-        new_action: th.Tensor,
-        mpc_cost_val: th.Tensor,
-        human_tuned_cost_val: th.Tensor,
-    ) -> th.Tensor:
-        """Compute the loss of the MPC parameter provider.
+    def parameter_jacobian(self, x: th.Tensor) -> th.Tensor:
+        """Compute the Jacobian of the DNN output wrt its parameters.
 
         Args:
-            param_increment (th.Tensor): The increment of the MPC parameters.
-            prev_action (th.Tensor): The previous action.
-            new_action (th.Tensor): The new action.
-            mpc_cost_val (th.Tensor): The MPC cost value.
-            human_tuned_cost_val (th.Tensor): The human-tuned cost value.
+            x (th.Tensor): The input tensor
 
         Returns:
-            th.Tensor: The loss of the MPC parameter provider.
+            th.Tensor: The Jacobian of the DNN output wrt its parameters
         """
-        Q_param = 1.0 * th.ones_like(param_increment)
-        action_diff = th.sum(th.pow(new_action - prev_action, 2))
-        loss = th.pow(param_increment - action_diff, 2) + th.pow(mpc_cost_val - human_tuned_cost_val, 2)
-        return loss
+        params = {k: v.detach() for k, v in self.named_parameters()}
+        buffers = {k: v.detach() for k, v in self.named_buffers()}
+
+        # n_dnn_params = sum(p.numel() for p in params.values())
+        # print(f"Total number of DNN MPC provider parameters: {n_dnn_params}")
+        def compute_sample_jacobian(sample):
+            # this will calculate the gradients for a single sample
+            # we want the gradients for each output wrt to the parameters
+            # this is the same as the jacobian of the model wrt the parameters
+
+            call = lambda x: th.func.functional_call(self, (x, buffers), sample)
+
+            # calculate the jacobian of the self.actor.mpc_param_provider wrt the parameters
+            J = th.func.jacrev(call)(params)
+
+            # J is a dictionary with keys the names of the parameters and values the gradients
+            # we want a tensor
+            grads = th.cat([v.flatten(1) for v in J.values()], -1)
+            return grads
+
+        # no we can use vmap to calculate the gradients for all samples at once
+        dnn_jacobians = th.vmap(compute_sample_jacobian)(x)
+        return dnn_jacobians
+
+    def loss_function_gradient(self, x: th.Tensor, dlag_mpc_dp: th.Tensor) -> th.Tensor:
+        """Compute the gradient of the loss function wrt the DNN parameters.
+
+        Args:
+            x (th.Tensor): Input features to the DNN
+            dlag_mpc_dp (th.Tensor): The Lagrangian jacobian wrt the MPC parameters (where p_MPC = p_DNN + p_mpc_old)
+
+        Returns:
+            th.Tensor: The gradient of the loss function wrt the DNN parameters
+        """
+        self_jac = self.parameter_jacobian(x)
+        param_increment = self(x)
+        print(f"MPC lagrangian jac: {dlag_mpc_dp}")
+        loss_grad = (self.parameter_weights @ param_increment + self.mpc_cost_val_scaling * dlag_mpc_dp) @ self_jac
+        print(f"Loss gradient: {loss_grad}")
+        return loss_grad
 
 
 class SACMPCActor(BasePolicy):
@@ -419,10 +453,14 @@ class SACMPCActor(BasePolicy):
         current_mpc_params = self.mpc_param_provider.normalize(current_mpc_params)
 
         for idx in range(batch_size):
-            prev_action = th.zeros(self.action_space.shape[0]) if state is None else state[idx]["norm_mpc_action"]
-            mpc_param_increment = (
-                self.mpc_param_provider(features[idx], current_mpc_params, prev_action).detach().numpy()
+            prev_action = (
+                th.zeros(self.action_space.shape[0])
+                if state is None
+                else th.from_numpy(state[idx]["norm_mpc_action"]).float()
             )
+            dnn_input = th.cat([features[idx], current_mpc_params, prev_action], dim=-1)
+            mpc_param_increment = self.mpc_param_provider(dnn_input)
+            mpc_param_increment = mpc_param_increment.detach().numpy()
 
             mpc_param_subset_dict = self.mpc_param_provider.map_to_parameter_dict(
                 mpc_param_increment, current_mpc_params.detach().numpy()
@@ -434,13 +472,16 @@ class SACMPCActor(BasePolicy):
             prev_soln = state[idx] if state is not None else None
             action, info = self.mpc.act(t=t, ownship_state=ownship_state, do_list=do_list, w=w, prev_soln=prev_soln)
             norm_action = self.action_type.normalize(action)
-            info.update({"unnorm_mpc_action": action, "norm_mpc_action": norm_action})
-            info.update({"dnn_input_features": features[idx].detach().cpu().numpy()})
             info.update(
                 {
+                    "prev_action": prev_action.numpy(),
+                    "unnorm_mpc_action": action,
+                    "norm_mpc_action": norm_action,
+                    "dnn_input_features": features[idx].detach().cpu().numpy(),
                     "mpc_param_increment": self.mpc_param_provider.unnormalize(mpc_param_increment),
                     "new_mpc_params": self.mpc.get_adjustable_mpc_params(),
                     "old_mpc_params": old_mpc_params,
+                    "old_mpc_params_norm": current_mpc_params.detach().numpy(),
                 }
             )
             if not deterministic:

rlmpc/sac.py
@@ -235,6 +235,8 @@ class SAC(opa.OffPolicyAlgorithm):
 
     def train(self, gradient_steps: int, batch_size: int = 64) -> None:
         # Switch to train mode (this affects batch norm / dropout)
+        batch_start_time = time.time()
+
         self.policy.set_training_mode(True)
         optimizers = [self.critic.optimizer, self.actor.optimizer]
         if self.ent_coef_optimizer is not None:
@@ -325,43 +327,29 @@ class SAC(opa.OffPolicyAlgorithm):
             min_qf_pi_sampled, _ = th.min(q_values_pi_sampled, dim=1, keepdim=True)
 
             self.actor.optimizer.zero_grad()
-            dnn_param_grads = []
-            jac_dnn_mpc_params = []
-            n_dnn_outputs = self.actor.mpc_param_provider.num_params
 
             # extract the parameters and buffers for a funcional call
-            actor_dnn_params = {k: v.detach() for k, v in self.actor.mpc_param_provider.named_parameters()}
-            actor_dnn_buffers = {k: v.detach() for k, v in self.actor.mpc_param_provider.named_buffers()}
             actor_dnn_feature_inputs = th.from_numpy(
                 np.array([info[0]["actor_info"]["dnn_input_features"] for info in replay_data.infos], dtype=np.float32)
             )
+            actor_dnn_old_mpc_param_inputs = th.from_numpy(
+                np.array([info[0]["actor_info"]["old_mpc_params"] for info in replay_data.infos], dtype=np.float32)
+            )
+            actor_dnn_prev_action_inputs = th.from_numpy(
+                np.array([info[0]["actor_info"]["prev_action"] for info in replay_data.infos], dtype=np.float32)
+            )
+            dnn_input = th.cat(
+                [actor_dnn_feature_inputs, actor_dnn_old_mpc_param_inputs, actor_dnn_prev_action_inputs], dim=1
+            )
 
-            # n_dnn_params = sum(p.numel() for p in actor_dnn_params.values())
-            # print(f"Total number of DNN MPC provider parameters: {n_dnn_params}")
-            def compute_sample_jacobian(sample):
-                # this will calculate the gradients for a single sample
-                # we want the gradients for each output wrt to the parameters
-                # this is the same as the jacobian of the model wrt the parameters
+            dnn_jacobians = self.actor.mpc_param_provider.parameter_jacobian(dnn_input)
 
-                # define a function that takes the as input returns the output of the self.actor.mpc_param_providerwork
-                call = lambda x: th.func.functional_call(self.actor.mpc_param_provider, (x, actor_dnn_buffers), sample)
-
-                # calculate the jacobian of the self.actor.mpc_param_providerwork wrt the parameters
-                J = th.func.jacrev(call)(actor_dnn_params)
-
-                # J is a dictionary with keys the names of the parameters and values the gradients
-                # we want a tensor
-                grads = th.cat([v.flatten(1) for v in J.values()], -1)
-                return grads
-
-            # no we can use vmap to calculate the gradients for all samples at once
-            dnn_jacobians = th.vmap(compute_sample_jacobian)(actor_dnn_feature_inputs)
-
-            actor_grads = th.zeros((batch_size, self.actor.num_params))
+            actor_grads = th.zeros((batch_size, self.actor.mpc_param_provider.num_params))
             actor_losses = th.zeros((batch_size, 1))
-            t_now = time.time()
+
             sens = self.policy.sensitivities()
             cov_inv = th.inverse(th.diag(th.exp(self.actor.log_std)))
+            t_actor_start_now = time.time()
             for b in range(batch_size):
                 actor_info = replay_data.infos[b][0]["actor_info"]
                 if not actor_info["optimal"]:
@@ -378,16 +366,14 @@ class SAC(opa.OffPolicyAlgorithm):
                     (cov_inv @ (sampled_actions[b] - norm_mpc_actions[b]).reshape(-1, 1)).T
                     @ da_dp_mpc
                     @ dnn_jacobians[b]
-                )
+                ).reshape(-1)
                 d_log_pi_da = -cov_inv @ (sampled_actions[b] - norm_mpc_actions[b])
                 df_repar_dp = da_dp_mpc @ dnn_jacobians[b]
 
                 dQ_da = th.autograd.grad(min_qf_pi_sampled[b], sampled_actions, create_graph=True)[0][b]
-                actor_grads[b] = ent_coef * d_log_pi_dp + ((ent_coef * d_log_pi_da - dQ_da) @ df_repar_dp).reshape(
-                    1, -1
-                )
+                actor_grads[b] = ent_coef * d_log_pi_dp + (ent_coef * d_log_pi_da - dQ_da) @ df_repar_dp
                 actor_losses[b] = ent_coef * sampled_log_prob[b] - min_qf_pi_sampled[b]
-            print("Actor gradient computation time: ", time.time() - t_now)
+            print("Actor gradient computation time: ", time.time() - t_actor_start_now)
             self.actor.update_params(-actor_grads.mean(dim=0) * self.lr_schedule(self._current_progress_remaining))
 
             if gradient_step % self.target_update_interval == 0:
@@ -397,12 +383,15 @@ class SAC(opa.OffPolicyAlgorithm):
 
         self._n_updates += gradient_steps
 
+        mean_actor_loss = actor_losses.clone().detach().mean().numpy()
+        mean_actor_grad_norm = np.linalg.norm(np.mean(actor_grads.clone().detach().numpy(), axis=0), ord=2)
+        print(
+            f"[TRAINING] Timesteps: {self.num_timesteps + 1} | Actor Loss: {mean_actor_loss:.4f} | Actor Grad Norm: {mean_actor_grad_norm:.4f} | Critic Loss: {np.mean(critic_losses):.4f} | Ent Coef Loss: {np.mean(ent_coef_losses):.4f} | Ent Coef: {np.mean(ent_coefs):.4f} | Batch processing time: {time.time() - batch_start_time:.2f}s"
+        )
         self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
         self.logger.record("train/ent_coef", np.mean(ent_coefs))
-        self.logger.record("train/actor_loss", actor_losses.clone().detach().mean().numpy())
-        self.logger.record(
-            "train/actor_grad_norm", np.linalg.norm(np.mean(actor_grads.clone().detach().numpy(), axis=0), ord=2)
-        )
+        self.logger.record("train/actor_loss", mean_actor_loss)
+        self.logger.record("train/actor_grad_norm", mean_actor_grad_norm)
         self.logger.record("train/critic_loss", np.mean(critic_losses))
         if len(ent_coef_losses) > 0:
             self.logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))

tests/generate_tracking_vae_training_data.py
@@ -127,11 +127,11 @@ if __name__ == "__main__":
         "show_loaded_scenario_data": False,
         "shuffle_loaded_scenario_data": True,
         "identifier": "training_env1",
-        "seed": 52,
+        "seed": 82,
     }
 
-    TRACKING_GRU_TRAINING_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland9.npy"
-    TRACKING_GRU_TEST_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland10.npy"
+    TRACKING_GRU_TRAINING_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland13.npy"
+    TRACKING_GRU_TEST_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland14.npy"
 
     use_vec_env = True
     if use_vec_env:
@@ -145,6 +145,7 @@ if __name__ == "__main__":
         tracking_observations = np.zeros((n_steps, *tracking_obs_dim), dtype=np.float32)
         for i in range(n_steps):
             actions = np.array([training_vec_env.action_space.sample() for _ in range(num_cpu)])
+            actions = np.zeros_like(actions)
             obs, reward, dones, info = training_vec_env.step(actions)
             # training_vec_env.render()
 
@@ -164,7 +165,7 @@ if __name__ == "__main__":
                 "max_number_of_episodes": 9000000,
                 "scenario_file_folder": test_scenario_folders,
                 "merge_loaded_scenario_episodes": True,
-                "seed": 56,
+                "seed": 86,
                 "test_mode": True,
                 "simulator_config": eval_sim_config,
                 "reload_map": False,
@@ -181,6 +182,8 @@ if __name__ == "__main__":
         tracking_observations = np.zeros((n_steps, *tracking_obs_dim), dtype=np.float32)
         for i in range(n_steps):
             actions = np.array([test_vec_env.action_space.sample() for _ in range(num_cpu)])
+            actions = np.zeros_like(actions)
+
             obs, reward, dones, info = test_vec_env.step(actions)
             test_vec_env.render()
 

tests/test_gym_sac_rlmpc.py
@@ -183,7 +183,7 @@ def main():
     actor_noise_std_dev = np.array([0.003, 0.003])  # normalized std dev for the action space [course, speed]
 
     mpc_param_provider_kwargs = {
-        "param_list": ["Q_p", "d_attenuation", "r_safe_do"],
+        "param_list": ["r_safe_do"],
         "hidden_sizes": [256],
         "activation_fn": th.nn.ReLU,
     }

tests/train_tracking_vae_attention.py
@@ -123,7 +123,7 @@ def train_vae(
 
             # extract length of valid obstacle observations
             seq_lengths = (
-                torch.sum(batch_obs[:, 0, :] < 0.95, dim=1).to("cpu").type(torch.int64)
+                torch.sum(batch_obs[:, 0, :] < 0.99, dim=1).to("cpu").type(torch.int64)
             )  # idx 0 is normalized distance, where vals = 1.0 is max dist of 1e4++ and thus not valid
             batch_obs = batch_obs.permute(0, 2, 1)  # permute to (batch, max_seq_len, input_dim)
 
@@ -182,7 +182,7 @@ def train_vae(
             batch_obs = batch_obs.to(device)
             # extract length of valid obstacle observations
             seq_lengths = (
-                torch.sum(batch_obs[:, 0, :] < 0.95, dim=1).to("cpu").type(torch.int64)
+                torch.sum(batch_obs[:, 0, :] < 0.99, dim=1).to("cpu").type(torch.int64)
             )  # idx 0 is normalized distance, where vals = 1.0 is max dist of 1e4++ and thus not valid
             batch_obs = batch_obs.permute(0, 2, 1)  # permute to (batch, max_seq_len, input_dim)
 
@@ -240,24 +240,24 @@ def train_vae(
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    latent_dims = [7, 10, 15]  # , 10, 15, 20]
+    latent_dims = [8, 10, 15]  # , 10, 15, 20]
     rnn_types = [torch.nn.GRU]
     num_rnn_layers_decoder = 1
-    rnn_hidden_dim_decoder = 64
+    rnn_hidden_dim_decoder = 100
     num_heads = 6
     embedding_dims = [240, 12, 48, 60, 72]  # [12, 24, 48]
     input_dim = 6
 
     load_model = False
     save_interval = 20
-    batch_size = 64
+    batch_size = 128
     num_epochs = 40
     learning_rate = 2e-4
 
     data_dir = Path("/home/doctor/Desktop/machine_learning/data/tracking_vae/")
     # data_dir = Path("/Users/trtengesdal/Desktop/machine_learning/data/vae/")
     training_data_filename_list = []
-    for i in range(1, 9):
+    for i in range(1, 15):
         training_data_filename = f"tracking_vae_training_data_rogaland{i}.npy"
         training_data_filename_list.append(training_data_filename)
 