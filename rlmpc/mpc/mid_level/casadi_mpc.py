Fix acados vs casadi sensitivity computation and verify LICQ are satisfied

rlmpc/mpc/mid_level/casadi_mpc.py
@@ -108,6 +108,8 @@ class CasadiMPC:
         self._idx_slacked_bx_constr: np.ndarray = np.array([])
         self._action_indices = [0, 1, 2]
 
+        self.g_str_list: list = []
+
         # debugging functions
         self._p_path = csd.MX.sym("p_path", 0)
         self._p_rate = csd.MX.sym("p_rate", 0)
@@ -395,6 +397,10 @@ class CasadiMPC:
         g_so_constr_vals = (
             self._static_obstacle_constraints(self._current_warmstart["x"], parameter_values).full().flatten()
         )
+        if g_so_constr_vals.size == 0:
+            g_so_constr_vals = np.array([0.0])
+        if g_do_constr_vals.size == 0:
+            g_do_constr_vals = np.array([0.0])
         if g_eq_vals.max() > 1e-6:
             print(
                 f"Warm start is infeasible wrt equality constraints at rows: {np.argwhere(np.abs(g_eq_vals) > 1e-6).flatten().T}!"
@@ -620,6 +626,14 @@ class CasadiMPC:
         X = []
         Sigma, Sigma_bx, Sigma_do, Sigma_so = [], [], [], []
 
+        hx_constr_str_list = []
+        hu_constr_str_list = []
+        hs_bx_constr_str_list = []
+        hs_so_constr_str_list = []
+        hs_do_constr_str_list = []
+        g_ineq_str_list = []
+        g_eq_str_list = []
+
         # if self._p_mdl.shape[0] > 0:
         #     p_adjustable.append(self._p_mdl)
 
@@ -632,8 +646,8 @@ class CasadiMPC:
         x_0 = csd.MX.sym("x_0_constr", nx, 1)
         x_k = csd.MX.sym("x_0", nx, 1)
         X.append(x_k)
-        hx.append(x_0 - x_k)  # x_0 <= x_k
-        hx.append(x_k - x_0)  # x_k <= x_0
+        g_eq_list.append(x_0 - x_k)  # x_k == x_0
+        g_eq_str_list.extend([f"x_0_constr{i} - x_0{i}" for i in range(nx)])
         p_fixed.append(x_0)
 
         self._idx_slacked_bx_constr = np.array([0, 1, 2, 3, 4, 5])
@@ -751,6 +765,8 @@ class CasadiMPC:
             U.append(u_k)
             hu.append(lbu_k - u_k)  # lbu <= u_k
             hu.append(u_k - ubu_k)  # u_k <= ubu
+            hu_constr_str_list.extend([f"lbu{l} <= u_" + str(k) + str(l) for l in range(nu)])
+            hu_constr_str_list.extend(["u_" + str(k) + f"{l} <= ubu{l}" for l in range(nu)])
 
             slack_penalty_cost = 0.0
             if k > 0:  # We do not slack the first state variable.
@@ -768,12 +784,24 @@ class CasadiMPC:
                 Sigma_bx.append(sigma_bxu_k)
                 hs_bx.append(-sigma_bxl_k)  # 0 <= sigma_bx_k
                 hs_bx.append(-sigma_bxu_k)  # 0 <= sigma_bx_k
+                hs_bx_constr_str_list.extend(
+                    ["-sigma_bxl_" + str(k) + f"{l} <= 0" for l in range(sigma_bxl_k.shape[0])]
+                )
+                hs_bx_constr_str_list.extend(
+                    ["-sigma_bxu_" + str(k) + f"{l} <= 0" for l in range(sigma_bxu_k.shape[0])]
+                )
                 hx.append(
                     lbx_k[self._idx_slacked_bx_constr] - x_k[self._idx_slacked_bx_constr] - sigma_bxl_k
                 )  # lbx - sigma_bx <= x_k
                 hx.append(
                     x_k[self._idx_slacked_bx_constr] - sigma_bxu_k - ubx_k[self._idx_slacked_bx_constr]
                 )  # x_k <= ubx + sigma_bx
+                hx_constr_str_list.extend(
+                    [f"lbx{l} - sigma_bx{k}{l} <= x_" + str(k) + str(l) for l in range(sigma_bxl_k.shape[0])]
+                )
+                hx_constr_str_list.extend(
+                    ["x_" + str(k) + f"{l} <= ubx{l} + sigma_bx{k}{l}" for l in range(sigma_bxu_k.shape[0])]
+                )
                 sigma_bx_k = csd.vertcat(sigma_bxl_k, sigma_bxu_k)
                 slack_penalty_cost += W_bx.T @ sigma_bx_k
 
@@ -791,10 +819,12 @@ class CasadiMPC:
             if max_num_so_constr > 0:
                 Sigma_so.append(sigma_so_k)
                 hs_so.append(-sigma_so_k)  # 0 <= sigma_so_k
+                hs_so_constr_str_list.extend(["-sigma_so_" + str(k) + f"{l} <= 0" for l in range(sigma_so_k.shape[0])])
                 slack_penalty_cost += W_so.T @ sigma_so_k
             if self._params.max_num_do_constr_per_zone > 0:
                 Sigma_do.append(sigma_do_k)
                 hs_do.append(-sigma_do_k)  # 0 <= sigma_do_k
+                hs_do_constr_str_list.extend(["-sigma_do_" + str(k) + f"{l} <= 0" for l in range(sigma_do_k.shape[0])])
                 slack_penalty_cost += W_do.T @ sigma_do_k
 
             x_path_k = self._x_path(x_k[4], self._x_path_coeffs)
@@ -853,16 +883,19 @@ class CasadiMPC:
             so_constr_k = self._create_static_obstacle_constraint(x_k, sigma_so_k, so_surfaces)
             so_constr_list.extend(so_constr_k)
             g_ineq_list.extend(so_constr_k)
+            g_ineq_str_list.extend(["h_so_" + str(k) + f"{l} <= 0" for l in range(len(so_constr_k))])
 
             X_do_k = csd.vertcat(X_do_cr[:, k], X_do_ho[:, k], X_do_ot[:, k])
             do_constr_k = self._create_dynamic_obstacle_constraint(x_k, sigma_do_k, X_do_k, nx_do, r_safe_do)
             do_constr_list.extend(do_constr_k)
             g_ineq_list.extend(do_constr_k)
+            g_ineq_str_list.extend(["h_do_" + str(k) + f"{l} <= 0" for l in range(len(do_constr_k))])
 
             x_k_end, _, _, _, _, _, _, _ = erk4(x_k, csd.vertcat(u_k, self._p_mdl))
             x_k = csd.MX.sym("x_" + str(k + 1), nx, 1)
             X.append(x_k)
             g_eq_list.append(x_k_end - x_k)
+            g_eq_str_list.extend(["F(x_" + str(k) + f"){l} - x_" + str(k + 1) + f"{l} = 0" for l in range(nx)])
 
             if k == 0:
                 print(
@@ -890,12 +923,20 @@ class CasadiMPC:
         Sigma_bx.append(sigma_bxu_k)
         hs_bx.append(-sigma_bxl_k)  # 0 <= sigma_bxl_k
         hs_bx.append(-sigma_bxu_k)  # 0 <= sigma_bxu_k
+        hs_bx_constr_str_list.extend(["-sigma_bxl_" + str(N) + f"{l} <= 0" for l in range(sigma_bxl_k.shape[0])])
+        hs_bx_constr_str_list.extend(["-sigma_bxu_" + str(N) + f"{l} <= 0" for l in range(sigma_bxu_k.shape[0])])
         hx.append(
             lbx_k[self._idx_slacked_bx_constr] - x_k[self._idx_slacked_bx_constr] - sigma_bxl_k
         )  # lbx - sigma_bxl <= x_k
         hx.append(
             x_k[self._idx_slacked_bx_constr] - sigma_bxu_k - ubx_k[self._idx_slacked_bx_constr]
         )  # x_k <= ubx + sigma_bxu
+        hx_constr_str_list.extend(
+            [f"lbx{l} - sigma_bxl{N}{l} <= x_" + str(N) + str(l) for l in range(sigma_bxl_k.shape[0])]
+        )
+        hx_constr_str_list.extend(
+            ["x_" + str(N) + f"{l} <= ubx{l} + sigma_bxu{N}{l}" for l in range(sigma_bxu_k.shape[0])]
+        )
         sigma_bx_k = csd.vertcat(sigma_bxl_k, sigma_bxu_k)
         slack_penalty_cost = W_bx.T @ sigma_bx_k
         if max_num_so_constr > 0:
@@ -906,6 +947,7 @@ class CasadiMPC:
             )
             Sigma_so.append(sigma_so_k)
             hs_so.append(-sigma_so_k)  # 0 <= sigma_so_k
+            hs_so_constr_str_list.extend(["-sigma_so_" + str(N) + f"{l} <= 0" for l in range(sigma_so_k.shape[0])])
             slack_penalty_cost += W_so.T @ sigma_so_k
         if self._params.max_num_do_constr_per_zone > 0:
             sigma_do_k = csd.MX.sym(
@@ -915,6 +957,7 @@ class CasadiMPC:
             )
             Sigma_do.append(sigma_do_k)
             hs_do.append(-sigma_do_k)  # 0 <= sigma_do_k
+            hs_do_constr_str_list.extend(["-sigma_do_" + str(N) + f"{l} <= 0" for l in range(sigma_do_k.shape[0])])
             slack_penalty_cost += W_do.T @ sigma_do_k
         print(
             f"dim lam_g_ineq_N = {2 * nx + len(do_constr_k) + len(so_constr_k) + (sigma_so_k.shape[0] + sigma_do_k.shape[0]) + 2 * n_bx_slacks}"
@@ -938,11 +981,13 @@ class CasadiMPC:
         so_constr_N = self._create_static_obstacle_constraint(x_k, sigma_so_k, so_surfaces)
         so_constr_list.extend(so_constr_N)
         g_ineq_list.extend(so_constr_N)
+        g_ineq_str_list.extend(["h_so_" + str(N) + f"{l} <= 0" for l in range(len(so_constr_N))])
 
         X_do_N = csd.vertcat(X_do_cr[:, N], X_do_ho[:, N], X_do_ot[:, N])
         do_constr_N = self._create_dynamic_obstacle_constraint(x_k, sigma_do_k, X_do_N, nx_do, r_safe_do)
         do_constr_list.extend(do_constr_N)
         g_ineq_list.extend(do_constr_N)
+        g_ineq_str_list.extend(["h_do_" + str(N) + f"{l} <= 0" for l in range(len(do_constr_N))])
 
         hs = hs_bx + hs_so + hs_do
 
@@ -980,6 +1025,15 @@ class CasadiMPC:
         self._g_ineq = g_ineq
         g = csd.vertcat(g_eq, g_ineq)
         self.ns_total = csd.vertcat(*Sigma).shape[0]
+        self.g_str_list = (
+            g_eq_str_list
+            + hu_constr_str_list
+            + hx_constr_str_list
+            + hs_bx_constr_str_list
+            + hs_so_constr_str_list
+            + hs_do_constr_str_list
+            + g_ineq_str_list
+        )
 
         # Create IPOPT solver object
         nlp_prob = {
@@ -1387,14 +1441,22 @@ class CasadiMPC:
         _, nu = self.model.dims()
         z = csd.vertcat(self._opt_vars, lamb, mu)
 
+        G_func = csd.Function("G_func", [self._opt_vars, self._p_fixed, self._p_adjustable], [G])
+        H_func = csd.Function("H_func", [self._opt_vars, self._p_fixed, self._p_adjustable], [H])
+
+        # Compute constraint jacobians
+        G_jac = csd.jacobian(G, self._opt_vars)
+        H_jac = csd.jacobian(H, self._opt_vars)
+        G_jac_func = csd.Function("G_jac_func", [self._opt_vars, self._p_fixed, self._p_adjustable], [G_jac])
+        H_jac_func = csd.Function("H_jac_func", [self._opt_vars, self._p_fixed, self._p_adjustable], [H_jac])
+
         # Build KKT matrix
         R_kkt = csd.vertcat(
             csd.transpose(dlag_dw),
             G,
             csd.diag(mu) @ H + tau,
         )
-        # check kkt conditions after
-        # # Generate sensitivity of the KKT matrix
+        # Generate sensitivity of the KKT matrix
         r_func = csd.Function("kkt_matrix_func", [z, self._p_fixed, self._p_adjustable], [R_kkt])
         dr_func = r_func.factory(
             "kkt_matrix_derivative_func", ["i0", "i1", "i2"], ["jac:o0:i0", "jac:o0:i1", "jac:o0:i2"]
@@ -1413,6 +1475,10 @@ class CasadiMPC:
         da_dp_func = csd.Function("da_dp_func", [z, self._p_fixed, self._p_adjustable], [da_dp])
         output_dict.update(
             {
+                "G": G_func,
+                "G_jac": G_jac_func,
+                "H": H_func,
+                "H_jac": H_jac_func,
                 "r_kkt": r_func,
                 "dr_dz": dR_dz_func,
                 "dr_dp": dr_dp_func,
@@ -1421,7 +1487,6 @@ class CasadiMPC:
                 "da_dp": da_dp_func,
             }
         )
-        # check how small the determinant of dr_dz isd
 
         # if generate_c_code:
         #     dr_dp_func.generate('dr_dp_func.c')

rlmpc/mpc/mid_level/mid_level_mpc.py
@@ -12,6 +12,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any, Dict, Optional, Tuple, Type
 
+import colav_simulator.common.math_functions as cs_mf
 import numpy as np
 import rlmpc.common.config_parsing as cp
 import rlmpc.common.paths as dp
@@ -21,6 +22,7 @@ import rlmpc.mpc.models as models
 import rlmpc.mpc.parameters as mpc_parameters
 import scipy.interpolate as interp
 import seacharts.enc as senc
+import sympy as sp
 
 uname_result = platform.uname()
 if uname_result.machine == "arm64" and uname_result.system == "Darwin":
@@ -265,23 +267,37 @@ class MidlevelMPC:
             mpc_soln_ac["soln"]["x"] = self._casadi_mpc.decision_variables(
                 mpc_soln_ac["inputs"], mpc_soln_ac["trajectory"], mpc_soln_ac["slacks"]
             ).full()
-        # else:
-        mpc_soln_csd = self._casadi_mpc.plan(
-            t,
-            xs,
-            do_cr_list,
-            do_ho_list,
-            do_ot_list,
-            warm_start,
-            perturb_nlp=perturb_nlp,
-            perturb_sigma=perturb_sigma,
-            **kwargs,
-        )
+        else:
+            mpc_soln_csd = self._casadi_mpc.plan(
+                t,
+                xs,
+                do_cr_list,
+                do_ho_list,
+                do_ot_list,
+                warm_start,
+                perturb_nlp=perturb_nlp,
+                perturb_sigma=perturb_sigma,
+                **kwargs,
+            )
         mpc_soln = mpc_soln_ac if self._acados_enabled else mpc_soln_csd
+
+        # self._check_optimality_conditions(mpc_soln_ac, mpc_soln_csd)
+
+        return mpc_soln
+
+    def _check_optimality_conditions(self, mpc_soln_ac: dict, mpc_soln_csd: dict) -> None:
+        """Check the optimality conditions for the acados and casadi solvers.
+
+        Args:
+            mpc_soln_ac (dict): Solution dictionary from the acados solver.
+            mpc_soln_csd (dict): Solution dictionary from the casadi solver.
+        """
         lam_g_ac = mpc_soln_ac["soln"]["lam_g"].flatten() if self._acados_enabled else np.array([])
         lam_g_csd = mpc_soln_csd["soln"]["lam_g"].flatten()
         lam_g_diff = lam_g_ac - lam_g_csd
-        w_diff = mpc_soln_ac["soln"]["x"].flatten() - mpc_soln_csd["soln"]["x"].flatten()
+        w_ac = mpc_soln_ac["soln"]["x"].flatten() if self._acados_enabled else np.array([])
+        w_csd = mpc_soln_csd["soln"]["x"].flatten()
+        w_diff = w_ac - w_csd
         z_ac = np.concatenate(
             (
                 mpc_soln_ac["soln"]["x"].flatten(),
@@ -297,4 +313,66 @@ class MidlevelMPC:
         R_kkt_ac = self.sens.r_kkt(z_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]).full()
         R_kkt_csd = self.sens.r_kkt(z_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]).full()
 
-        return mpc_soln
+        dlag_dw_ac = self.sens.dlag_dw(
+            mpc_soln_ac["soln"]["x"], lam_g_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]
+        ).full()
+        dlag_dw_csd = self.sens.dlag_dw(
+            mpc_soln_csd["soln"]["x"], lam_g_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]
+        ).full()
+        print("Checking first order necessary conditions for optimality...: ")
+        print(
+            "dlag_dw = 0 condition | acados: ", np.linalg.norm(dlag_dw_ac), " | casadi: ", np.linalg.norm(dlag_dw_csd)
+        )
+
+        eq_constr_ac = self.sens.G(w_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]).full()
+        eq_jac_ac = self.sens.G_jac(w_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]).full()
+        ineq_constr_ac = self.sens.H(w_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]).full()
+        ineq_jac_ac = self.sens.H_jac(w_ac, mpc_soln_ac["p_fixed"], mpc_soln_ac["p"]).full()
+        n_eq_constr = eq_jac_ac.shape[0]
+
+        eq_constr_csd = self.sens.G(w_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]).full()
+        eq_jac_csd = self.sens.G_jac(w_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]).full()
+        ineq_constr_csd = self.sens.H(w_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]).full()
+        ineq_jac_csd = self.sens.H_jac(w_csd, mpc_soln_csd["p_fixed"], mpc_soln_csd["p"]).full()
+
+        print(
+            f"c_i(x*) = 0 for all i in E | acados: {np.all(np.abs(eq_constr_ac) < 1e-4)} | casadi: {np.all(np.abs(eq_constr_csd) < 1e-4)}"
+        )
+        print(
+            f"c_i(x*) <= 0 for all i in I | acados: {np.all(ineq_constr_ac < 1e-4)} | casadi: {np.all(ineq_constr_csd < 1e-4)}"
+        )
+        active_constraints_ac = np.where(np.abs(np.concatenate((eq_constr_ac, ineq_constr_ac))) < 1e-4)[0]
+        active_constraints_csd = np.where(np.abs(np.concatenate((eq_constr_csd, ineq_constr_csd))) < 1e-4)[0]
+
+        active_constr_jac_ac = np.concatenate((eq_jac_ac, ineq_jac_ac), axis=0)[active_constraints_ac, :]
+        rank_active_constr_jac_ac = np.linalg.matrix_rank(active_constr_jac_ac)
+        max_rank_ac = np.min(active_constr_jac_ac.shape)
+        active_constr_jac_csd = np.concatenate((eq_jac_csd, ineq_jac_csd), axis=0)[active_constraints_csd, :]
+        rank_active_constr_jac_csd = np.linalg.matrix_rank(active_constr_jac_csd)
+        max_rank_csd = np.min(active_constr_jac_csd.shape)
+
+        # dep_rows_ac = cs_mf.find_dependent_rows(active_constr_jac_ac.copy())
+        # dep_rows_csd = cs_mf.find_dependent_rows(active_constr_jac_csd.copy())
+        # for dep in dep_rows_ac:
+        #     print(self._casadi_mpc.g_str_list[active_constraints_ac[dep]])
+
+        print(
+            f"Lamda_i >= 0 for i in I | acados: {np.all(lam_g_ac[n_eq_constr:] >= 0)} | casadi: {np.all(lam_g_csd[n_eq_constr:] >= 0)}"
+        )
+        print(
+            f"Lambda_i * c_i(x*) = 0 for all i in (I and E) | acados: {np.all(lam_g_ac * np.concatenate((eq_constr_ac, ineq_constr_ac)).flatten() < 1e-4)} | casadi: {np.all(lam_g_csd * np.concatenate((eq_constr_csd, ineq_constr_csd)).flatten() < 1e-4)}"
+        )
+
+        print(
+            f"Active constraint jacobian rank/max_rank | acados: {rank_active_constr_jac_ac}/{max_rank_ac} | casadi: {rank_active_constr_jac_csd}/{max_rank_csd}"
+        )
+
+        # print("Checking second order necessary conditions for optimality: ")
+        # hess_ac = self.sens.d2lag_d2w(
+        #     w_ac, np.concatenate((mpc_soln_ac["p"], mpc_soln_ac["p_fixed"])), 1.0, lam_g_ac
+        # ).full()
+        # hess_csd = self.sens.d2lag_d2w(
+        #     w_csd, np.concatenate((mpc_soln_csd["p"], mpc_soln_csd["p_fixed"])), 1.0, lam_g_csd
+        # ).full()
+
+        print("Hessian >= 0 on null space of equality constraints: ")

tests/generate_tracking_vae_training_data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from sys import platform
 from typing import Callable, Tuple
 
+import colav_simulator.behavior_generator as cs_bg
 import colav_simulator.scenario_generator as cs_sg
 import colav_simulator.simulator as cs_sim
 import gymnasium as gym
@@ -65,10 +66,13 @@ if __name__ == "__main__":
     if generate:
         scenario_generator = cs_sg.ScenarioGenerator(config_file=rl_dp.config / "scenario_generator.yaml")
         for idx, name in enumerate(scenario_names):
-            if idx == 1:
+            if idx == 0:
                 continue
 
-            scenario_generator.seed(idx + 1)
+            scenario_generator.behavior_generator.set_ownship_method(
+                cs_bg.BehaviorGenerationMethod.ConstantSpeedAndCourse
+            )
+            scenario_generator.seed(idx)
             _ = scenario_generator.generate(
                 config_file=rl_dp.scenarios / (name + ".yaml"),
                 new_load_of_map_data=False if idx == 0 else False,
@@ -76,7 +80,7 @@ if __name__ == "__main__":
                 save_scenario_folder=rl_dp.scenarios / "training_data" / name,
                 show_plots=True,
                 episode_idx_save_offset=0,
-                n_episodes=90,
+                n_episodes=200,
                 delete_existing_files=True,
             )
 
@@ -88,7 +92,7 @@ if __name__ == "__main__":
                 save_scenario_folder=rl_dp.scenarios / "test_data" / name,
                 show_plots=True,
                 episode_idx_save_offset=0,
-                n_episodes=20,
+                n_episodes=50,
                 delete_existing_files=True,
             )
 
@@ -127,11 +131,11 @@ if __name__ == "__main__":
         "show_loaded_scenario_data": False,
         "shuffle_loaded_scenario_data": True,
         "identifier": "training_env1",
-        "seed": 82,
+        "seed": 0,
     }
 
-    TRACKING_GRU_TRAINING_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland13.npy"
-    TRACKING_GRU_TEST_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland14.npy"
+    TRACKING_GRU_TRAINING_DATA_SAVE_FILE = "tracking_vae_training_data_rogaland1.npy"
+    TRACKING_GRU_TEST_DATA_SAVE_FILE = "tracking_vae_test_data_rogaland1.npy"
 
     use_vec_env = True
     if use_vec_env:
@@ -165,7 +169,7 @@ if __name__ == "__main__":
                 "max_number_of_episodes": 9000000,
                 "scenario_file_folder": test_scenario_folders,
                 "merge_loaded_scenario_episodes": True,
-                "seed": 86,
+                "seed": 1,
                 "test_mode": True,
                 "simulator_config": eval_sim_config,
                 "reload_map": False,
@@ -178,7 +182,7 @@ if __name__ == "__main__":
         observations = [obs]
         frames = []
         tracking_obs_dim = list(obs["RelativeTrackingObservation"].shape)
-        n_steps = 1100
+        n_steps = 500
         tracking_observations = np.zeros((n_steps, *tracking_obs_dim), dtype=np.float32)
         for i in range(n_steps):
             actions = np.array([test_vec_env.action_space.sample() for _ in range(num_cpu)])