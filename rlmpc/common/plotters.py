3993757bed15ce9c9674e2ec6e8b79773693b97e
Trym Tengesdal <tengesdal1994@gmail.com>
2024-09-16 12:57:42 +0200

    Change eval mdl save in cb

--- rlmpc/common/plotters.py
+++ rlmpc/common/plotters.py
@@ -8,7 +8,7 @@
 """
 
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 import colav_simulator.common.image_helper_methods as ihm
 import colav_simulator.gym.logger as csenv_logger
@@ -365,6 +365,66 @@ def plot_single_model_training_stats(
     plt.show(block=False)
 
 
+def plot_multiple_model_eval_results(
+    eval_data_list: List[Dict[str, Any]], model_names: List[str], save_fig: bool = False, save_path: Path = None
+) -> None:
+    fig, axs = plt.subplots(2, 1, figsize=(15, 10), num="mm_eval_results")
+    fig.subplots_adjust(hspace=0.3, wspace=0.25)
+    colors = sns.color_palette("tab10", n_colors=len(eval_data_list))
+    for name, data in zip(model_names, eval_data_list):
+        color = colors.pop()
+        axs.plot(data["timesteps"], data["ep_lengths"], label=name, color=color)
+        axs.plot(data["timesteps"], data["results"], label=name, color=color)
+
+    if save_fig:
+        save_path = save_path if save_path is not None else Path("./")
+        if not save_path.exists():
+            save_path.mkdir(parents=True)
+        fig.savefig(save_path / "mm_eval_results.pdf", bbox_inches="tight", dpi=100)
+
+
+def plot_multiple_model_worst_and_best_episode_data(
+    wb_env_data_list: List[Tuple[csgym_logger.EpisodeData, csgym_logger.EpisodeData]],
+    model_names: List[str],
+    save_fig: bool = False,
+    save_path: Path = None,
+) -> None:
+    """Plots the worst and best episode data for each model in the list.
+
+    Args:
+        wb_env_data_list (List[Tuple[csgym_logger.EpisodeData, csgym_logger.EpisodeData]]): Worst and best episode data for each model.
+        model_names (List[str]): List of model names.
+        save_fig (bool, optional): Whether to save the figure.
+        save_path (Path, optional): Path to save the figure.
+    """
+    fig_worst, axs_worst = plt.subplots(2, 2, figsize=(15, 10), num="worst_episode_data")
+    fig_worst.subplots_adjust(hspace=0.3, wspace=0.25)
+    fig_best, axs_best = plt.subplots(2, 2, figsize=(15, 10), num="best_episode_data")
+    fig_best.subplots_adjust(hspace=0.3, wspace=0.25)
+    colors = sns.color_palette("tab10", n_colors=len(wb_env_data_list))
+    for name, data in zip(model_names, wb_env_data_list):
+        color = colors.pop()
+        plot_episode_data_series(axs_worst, data[0], name + "_worst_ep", color=color)
+        plot_episode_data_series(axs_best, data[1], name + "_best_ep", color=color)
+    if save_fig:
+        save_path = save_path if save_path is not None else Path("./")
+        if not save_path.exists():
+            save_path.mkdir(parents=True)
+        fig_worst.savefig(save_path / "worst_episode_data.pdf", bbox_inches="tight", dpi=100)
+        fig_best.savefig(save_path / "best_episode_data.pdf", bbox_inches="tight", dpi=100)
+
+
+def plot_episode_data_series(axs: plt.Axes, data: csgym_logger.EpisodeData, name: str, color: str = None) -> None:
+    """Plots the episode data series.
+
+    Args:
+        axs (plt.Axes): Axes to plot on.
+        data (csgym_logger.EpisodeData): Episode data to plot.
+        name (str): Name of the model + episode used in the label
+        color (str, optional): Optional color for the plot.
+    """
+
+
 def plot_training_results(base_dir: Path, experiment_names: List[str]) -> None:
     """Plots results from training.
 
@@ -428,20 +488,48 @@ def plot_evaluation_results(base_dir: Path, experiment_names: List[str]) -> None
         experiment_names (List[str]): List of experiment names (experiment folder names).
     """
     env_data_list = []
-    reward_data_list = []
+    wb_env_data_list = []
+    eval_data_list = []
 
     plot_env_snapshots = False
-    plot_reward_curves = True
+    plot_evaluation_curves = True
+    plot_worst_and_best_episode_data = True
     for experiment_name in experiment_names:
         log_dir = base_dir / experiment_name
+        eval_data_dir = log_dir / "eval_data"
 
-        if plot_env_snapshots or plot_reward_curves:
-            env_logger = csenv_logger.Logger(experiment_name=experiment_name, log_dir=log_dir)
-            env_logger.load_from_pickle(f"{experiment_name}_env_data")
-            env_data_list.append(env_logger.env_data)
+        eval_data = {}
+        eval_data["timesteps"] = []
+        eval_data["ep_lengths"] = []
+        eval_data["results"] = []
+        npz_file_list = [file for file in eval_data_dir.iterdir()]
+        npz_file_list = [file for file in npz_file_list if file.suffix == ".npz"]
+        npz_file_list.sort(key=lambda x: int(x.stem.split("_")[-1]))
+        indices_with_above_100_ep_lengths = []
+        for npzf in npz_file_list:
+            with np.load(npzf) as data:
+                eval_data["timesteps"].append(int(data["timesteps"].item()))
+                eval_data["ep_lengths"].append(int(data["ep_lengths"].item()))
+                eval_data["results"].append(float(data["results"].item()))
+                if int(data["ep_lengths"].item()) > 100:
+                    indices_with_above_100_ep_lengths.append(int(data["timesteps"].item()))
+        eval_data_list.append(eval_data)
 
-            reward_data = hf.extract_reward_data(env_logger.env_data)
-            reward_data_list.append(reward_data)
+        env_data_pkl_file_list = [file for file in eval_data_dir.iterdir()]
+        env_data_pkl_file_list = [eval_data_dir / file.stem for file in env_data_pkl_file_list if file.suffix == ".pkl"]
+        env_data_pkl_file_list.sort(key=lambda x: int(x.stem.split("_")[-3]))
+
+        env_logger = csenv_logger.Logger(experiment_name=experiment_name, log_dir=log_dir)
+        # Plot mpc params and actions for best and worst episodes
+
+        if plot_worst_and_best_episode_data:
+            argmin_reward = int(np.argmin(eval_data["results"]))
+            argmax_reward = int(np.argmax([eval_data["results"][i] for i in indices_with_above_100_ep_lengths]))
+            env_logger.load_from_pickle(str(env_data_pkl_file_list[argmin_reward]))
+            worst_env_data = env_logger.env_data[0]
+            best_env_data = env_logger.load_from_pickle(str(env_data_pkl_file_list[argmax_reward]))
+            best_env_data = env_logger.env_data[0]
+            wb_env_data_list.append((worst_env_data, best_env_data))
 
         if plot_env_snapshots:
             plot_single_model_enc_snapshots(
@@ -452,18 +540,25 @@ def plot_evaluation_results(base_dir: Path, experiment_names: List[str]) -> None
                 save_path=base_dir / experiment_name / "figures",
             )
 
-    if plot_reward_curves:
-        plot_multiple_model_reward_curves(
-            reward_data_list,
+    if plot_evaluation_curves:
+        plot_multiple_model_eval_results(
+            eval_data_list=eval_data_list,
             model_names=experiment_names,
             save_fig=True,
-            save_path=base_dir / experiment_name / "figures",
+            save_path=base_dir / "figures",
+        )
+    if plot_worst_and_best_episode_data:
+        plot_multiple_model_worst_and_best_episode_data(
+            wb_env_data_list=wb_env_data_list,
+            model_names=experiment_names,
+            save_fig=True,
+            save_path=base_dir / "figures",
         )
 
 
 if __name__ == "__main__":
     base_dir: Path = Path.home() / "Desktop/machine_learning/rlmpc"
     experiment_names = ["snmpc_db_1te_1ee_16cpus"]
-    plot_training_results(base_dir=base_dir, experiment_names=experiment_names)
-    # plot_evaluation_results(base_dir=base_dir, experiment_names=experiment_names)
+    # plot_training_results(base_dir=base_dir, experiment_names=experiment_names)
+    plot_evaluation_results(base_dir=base_dir, experiment_names=experiment_names)
     print("Done plotting")
