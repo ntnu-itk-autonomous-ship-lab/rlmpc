e6141c671161bdc55c80ff1e6b7e894ed4a73bc9
Trym Tengesdal <tengesdal1994@gmail.com>
2024-09-16 10:01:09 +0200

    Change cb freqs

--- rlmpc/scripts/train_rlmpc_sac.py
+++ rlmpc/scripts/train_rlmpc_sac.py
@@ -89,7 +89,7 @@ def train_rlmpc_sac(
         log_dir=base_dir,
         model_dir=model_dir,
         experiment_name=experiment_name,
-        save_stats_freq=200,
+        save_stats_freq=1000,
         save_agent_model_freq=5000,
         log_freq=n_training_envs,
         max_num_env_episodes=2000,
